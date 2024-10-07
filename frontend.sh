8ffd51a604bbea6fa56559020e284ad970d5e897
pavandurga <pavandurga071981@gmail.com>
2024-10-08 00:45:51 +0100

    craeted a front script.

---
+++ frontend.sh
@@ -0,0 +1,7 @@
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
